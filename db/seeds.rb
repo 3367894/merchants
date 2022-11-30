cc3442b842d1f2668fe9f61c129d521530f6bbcd
Kirill <3367894@gmail.com>
2022-11-30 12:13:32 +0100

    initial models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+path = Rails.root.join("db", "seeds_data")
+
+%w[merchant shopper order].each do |entity_name|
+  klass = entity_name.classify.constantize
+
+  CSV.read(path.join("#{entity_name.pluralize}.csv"), headers: true).each do |data|
+    entity = klass.find_by(id: data["id"])
+    klass.create!(data) unless entity
+  end
+end
