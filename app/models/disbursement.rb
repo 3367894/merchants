813e7ded7ae7c46df54d55d5a7387e580f803d75
Kirill <3367894@gmail.com>
2022-11-30 21:02:49 +0100

    validations for models

--- app/models/disbursement.rb
+++ app/models/disbursement.rb
@@ -1,3 +1,8 @@
 class Disbursement < ApplicationRecord
   belongs_to :merchant
+
+  validates :merchant_id, presence: true
+  validates :year, presence: true
+  validates :week, presence: true, uniqueness: { scope: [:year, :merchant_id] }
+  validates :amount_cents, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :shopper
   belongs_to :merchant
+
+  validates :amount_cents, presence: true
 end
